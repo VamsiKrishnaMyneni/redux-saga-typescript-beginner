481e1efd14ee665ed904ccc95d6402290620f0b7
Vamsi Krishna Myneni <32959424+VamsiKrishnaMyneni@users.noreply.github.com>
2025-03-04 21:46:50 +0530

    changed folder strucure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Counter from './Counter/components/Counter';
 import Users from './Users/components/Users';
 import './App.css';
 
 function App() {
   const { count } = useSelector((state: any) => state.counter);
-  const dispatch = useDispatch();
-
 
   return (
     <div className="App">
-      <button onClick={() => dispatch({ type: "INCREMENT", count: count + 1 })}>+</button>
+
       <Counter count={count} />
-      <button onClick={() => dispatch({ type: "DECREMENT", count: count - 1 })}>-</button>
+
       <Users />
     </div>
   );
